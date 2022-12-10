fa4c98f20e9f0998d49e0853873c7a9d20efec1a
Alex Phillip <alexjoelphillip@gmail.com>
2022-12-10 12:43:15 +0000

    completed 3.5

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
+const { query } = require("express");
 const express = require("express");
 
 const app = express();
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Yay Node!");
-  console.log(req);
-});
+// app.get("/", (req, res) => {
+//   res.send("Yay Node!");
+//   console.log(req);
+// });
 
 app.get("/glide", (req, res) => {
   res.send("Gliding is nice :)");
   console.log(req);
+});
+
+app.get("/plaice", (req, res) => {
+    res.send("Plaice is nice :)");
+});
+
+app.get("/", (req, res) => {
+    let search = req.query.search;
+    res.send("Hello world, you searched for " + search);
 });
 
 app.listen(port, () => {
